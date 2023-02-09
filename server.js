a82a0065bbc66a2e0472498e0320a3b488108870
Paul <juniorrivera81@yahoo.com>
2023-02-09 09:56:20 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const http = require("http");
+
+const port = 3000;
+
+// Creates a server object
+http
+  .createServer((request, response) => {
+    const url = request.url;
+    const method = request.method;
+    response.setHeader("Content-Type", "text/html");
+
+    console.log("Request made with this method: ", request.method);
+    const dataChunksArray = [];
+
+    request.on("data", (chunk) => {
+      console.log("Chunk", chunk);
+      dataChunksArray.push(chunk);
+    });
+    request.on("end", () => {
+      const body = JSON.parse(Buffer.concat(dataChunksArray).toString());
+      const responseBody = { method, url, body };
+
+      response.write(JSON.stringify(responseBody));
+      response.end();
+    });
+    if (url == "/") {
+      response.write("<h1>Home: Paul's Pad</h1");
+      response.statusCode = 200;
+    } else if (url == "/about") {
+      response.statusCode = 200;
+      response.write("About - I am Paul");
+    } else if (url == "/contact") {
+      response.statusCode = 200;
+      response.write("<h1>Contact Me: paul@fakemail.net</h1");
+    }
+    response.end();
+  })
+  .listen(port, () => {
+    console.log("Server listening on port: " + port);
+  });
